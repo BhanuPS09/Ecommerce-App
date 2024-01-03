8e840fba8eae1e731cec5907e4ce30235643f908
Bhanu Pratap Singh <bhanusinghpcm09@gmail.com>
2024-01-03 12:10:31 +0530

    Home routes are fixed

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,26 @@
 import "./directory-component.scss";
+import { Link } from "react-router-dom";
 
 const Directory = (props) => {
-    const { properties:{id,title,imageUrl} } = props;
-    return (
-        <div className="directory-item-container" key={id}>
-            <div className="background-image" style={{
-                backgroundImage: `url(${imageUrl})`
-            }}></div>
-            <div className="body">
-                <h2>{title}</h2>
-                <p>Shop Now</p>
-            </div>
-
-        </div>
-    );
-
-}
-
+  const {
+    properties: { id, title, imageUrl },
+  } = props;
+  return (
+    <div className="directory-item-container" key={id}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="body">
+        <Link to={`shop/${title}`}>
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Directory;
