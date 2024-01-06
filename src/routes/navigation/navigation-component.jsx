d3a39d95a3f15a29338743dd6b75891aef155a5a
Bhanu Pratap Singh <bhanusinghpcm09@gmail.com>
2024-01-06 13:43:42 +0530

    Home UI fix

--- src/routes/navigation/navigation-component.jsx
+++ src/routes/navigation/navigation-component.jsx
@@ -26,7 +26,8 @@ const Navigation = () => {
             <div className="navigation">
                 <Link className="logo-container" to="/">
                     <div>
-                        <CrwnLogo className="logo" />
+                        {/* <CrwnLogo className="logo" /> */}
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28"><path d="M19 21H5C4.44772 21 4 20.5523 4 20V11L1 11L11.3273 1.6115C11.7087 1.26475 12.2913 1.26475 12.6727 1.6115L23 11L20 11V20C20 20.5523 19.5523 21 19 21ZM6 19H18V9.15745L12 3.7029L6 9.15745V19Z" fill="currentColor"></path></svg>
                     </div>
                 </Link>
 
